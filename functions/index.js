a28490386a0e0adb803afd8a1ee76011511259ee
Nara <Naroin.Kil@gmail.com>
2025-08-17 22:43:03 +0300

    modified:   functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,41 @@
+// functions/index.js
 const { onCall } = require("firebase-functions/v2/https");
 const { initializeApp } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 
 initializeApp();
 
-// registerUser: создаём/находим пользователя по нику и отдаём custom token
+// Создать/найти юзера по нику и выдать custom token
 exports.registerUser = onCall(async (req) => {
   const { nick } = req.data || {};
-  if (!nick || typeof nick !== "string") throw new Error("nick is required");
-
-  let user;
-  try {
-    user = await getAuth().getUser(nick);
-  } catch (e) {
-    user = await getAuth().createUser({ uid: nick, displayName: nick });
+  if (!nick || typeof nick !== "string") {
+    throw new Error("nick is required");
   }
-  const token = await getAuth().createCustomToken(user.uid);
-  return { token };
-});
 
-// loginUser: выдаём custom token по нику (пин можно прикрутить позже)
-exports.loginUser = onCall(async (req) => {
-  const { nick } = req.data || {};
-  if (!nick || typeof nick !== "string") throw new Error("nick is required");
+  try {
+    await getAuth().getUser(nick);
+  } catch (e) {
+    // если пользователя нет — создаём
+    await getAuth().createUser({ uid: nick, displayName: nick });
+  }
+
   const token = await getAuth().createCustomToken(nick);
   return { token };
 });
 
-// createCharacter: заглушка, вернёт id
-exports.createCharacter = onCall(async () => {
+// Выдать custom token по нику
+exports.loginUser = onCall(async (req) => {
+  const { nick } = req.data || {};
+  if (!nick || typeof nick !== "string") {
+    throw new Error("nick is required");
+  }
+  const token = await getAuth().createCustomToken(nick);
+  return { token };
+});
+
+// Заглушка создания персонажа
+exports.createCharacter = onCall(async (req) => {
+  // можешь валидировать req.auth и req.data при желании
   const id = "char-" + Date.now();
   return { id };
 });
